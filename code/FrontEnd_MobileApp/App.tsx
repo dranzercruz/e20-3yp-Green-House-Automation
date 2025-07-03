3723f78259a59ce08a5b754006538ede2cac52ac
Eniyavan <e20099@eng.pdn.ac.lk>
2025-07-03 20:24:56 +0530

    notification feature added

--- code/FrontEnd_MobileApp/App.tsx
+++ code/FrontEnd_MobileApp/App.tsx
@@ -1,18 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { useState } from 'react';
+
 import Page from './app/index';
 import login from './app/Components/Authentication/login';
 import register from './app/Components/Authentication/register';
 
+import { initializeNotifications } from './app/Components/Notification Handler/NotificationHandler';
+
 const Stack = createStackNavigator();
 
 export default function App() {
-  const [isLogin, setisLogin] = useState(false);
-  
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    let unsubscribe: () => void;
+
+    (async () => {
+      unsubscribe = await initializeNotifications();
+    })();
+
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={isLogin? "page": "login"}>
+      <Stack.Navigator initialRouteName={isLogin ? "page" : "login"}>
         <Stack.Screen name="login" component={login} />
         <Stack.Screen name="page" component={Page} />
         <Stack.Screen name="register" component={register} />
